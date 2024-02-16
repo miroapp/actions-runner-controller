ec8bca91cc40f744826a741aad5da778bfef486e
Kyrylo Bilchenko <kyrylo@miro.com>
2024-02-16 13:32:42 +0100

    Adjust starting time statement to parse the case where no message about job is about to start (#5)
    * Adjust starting time statement to parse the case where no message about job is about to start
    
    * update certs from upstream to make build pass
    
    ---------
    
    Co-authored-by: kyrylomiro <kyrylomiro@users.noreply.github.com>

--- pkg/actionsmetrics/event_reader.go
+++ pkg/actionsmetrics/event_reader.go
@@ -273,9 +273,11 @@ func (reader *EventReader) fetchAndParseWorkflowJobLogs(ctx context.Context, e *
 				continue
 			}
 
-			if strings.HasPrefix(line, "Job is about to start running on the runner:") {
-				startedTime, _ = time.Parse(time.RFC3339, timestamp)
-				continue
+			if strings.HasPrefix(line, "Job is about to start running on the runner:") || strings.HasPrefix(line, "Current runner version:") {
+				if startedTime.UTC().IsZero() {
+					startedTime, _ = time.Parse(time.RFC3339, timestamp)
+					continue
+				}
 			}
 
 			// Last line in the log will count as the completed time
